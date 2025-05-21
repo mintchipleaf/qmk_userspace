79279f34daea83320a78b0b3919f49babe51c3c2
mintchipleaf <mintchipleaf@gmail.com>
2025-05-20 17:42:51 -0700

    Add quantum painter support, disable oled, fix mouse layer relying on oled def

--- keyboards/splitkb/kyria/keymaps/mint/rules.mk
+++ keyboards/splitkb/kyria/keymaps/mint/rules.mk
@@ -1,9 +1,15 @@
-OLED_ENABLE = yes
+OLED_ENABLE = no
 ENCODER_ENABLE = yes       # Enables the use of one or more encoders
 RGB_MATRIX_ENABLE = no     # Disable keyboard RGB matrix, as it is enabled by default on rev3
 RGBLIGHT_ENABLE = no       # Enable keyboard RGB underglow
 POINTING_DEVICE_DRIVER = cirque_pinnacle_i2c
 POINTING_DEVICE_ENABLE = yes
 WPM_ENABLE = yes
+QUANTUM_PAINTER_ENABLE = yes
+QUANTUM_PAINTER_DRIVERS += sh1106_i2c
+QUANTUM_PAINTER_DRIVERS += surface
 
-CONVERT_TO=kb2040
+SRC += carbonthin24.qff.c
+SRC += nostromo-outline.qgf.c
+
+CONVERT_TO=liatris
